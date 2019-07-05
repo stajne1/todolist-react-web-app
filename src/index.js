983379926973f5722c5e6f5975d9d7d1655ed587
Sandip Tajne <stajne1@gmail.com>
2019-07-05 21:26:15 +0530

    Store Setup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createSagaMiddleWare } from 'redux-saga';
+import Reducer from './reducer';
+
+const sagaMiddleware = createSagaMiddleWare();
+
+const store = createStore(Reducer, applyMiddleware(sagaMiddleware));
+
+render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
